dc6ad32ab750fd5a472edd8577d76d3c51231934
KRIS DAVIS <krissdavis18@mail.com>
2019-06-02 14:40:11 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,12 @@
 def using_while
 	levitation_force = 6
 	
-	#your code here
+while "levitation_force = 6"
+  levitation_force +=1
+ puts "you have now raised Wingardium Leviosa += 1"
+ break
+end
+	
 end
 
 
